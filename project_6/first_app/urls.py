ab1eeec6ca7212d5eefaf0bbaa15a57492acc450
Amit Barua <amitbarua737@gmail.com>
2025-07-29 18:12:19 +0600

    project seven added

---
+++ project_6/first_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    
+    path('', views.index, name = 'home'),
+    path('about/<int:id>', views.about, name = 'about'),
+
+]
