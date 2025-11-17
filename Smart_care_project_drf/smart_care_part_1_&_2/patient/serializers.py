2b9a4646c1db579ad171634821be6a2d61150751
amit_barua <157397524+student-amitbarua@users.noreply.github.com>
2025-11-17 18:47:16 +0600

    Delete Smart_care_project_drf directory

--- Smart_care_project_drf/smart_care_part_1_&_2/patient/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-
-class PatientSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(many=False) # ek model theke arek model er data collect kore show kora 
-    class Meta:
-        model = models.Patient
-        fields = '__all__'
